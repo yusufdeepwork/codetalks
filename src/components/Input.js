1bb3abd4b0f2613207e22c206a4ea8651fcc497e
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-07-23 10:42:47 +0300

    Add Input component

---
+++ src/components/Input.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, TextInput} from 'react-native';
+
+const Input = ({text, isSecure, value}) => {
+  return (
+    <TextInput
+      value={value}
+      placeholder={text}
+      style={styles.input}
+      placeholderTextColor={'white'}
+      secureTextEntry={isSecure}
+    />
+  );
+};
+
+export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    color: 'white',
+    borderBottomColor: 'white',
+    borderBottomWidth: 1,
+    margin: 10,
+    textDecorationLine: 'none',
+  },
+});
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Input from '../components/Input';
 
 const Login = () => {
   return (
     <View style={styles.container}>
       <Text>Code Talks</Text>
+      <Input text="e-postanızı giriniz" />
+      <Input text="şifrenizi giriniz" isSecure />
     </View>
   );
 };
