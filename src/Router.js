83106a9bbaa2730cbc76b1adbc72e87a7e60fe35
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-07-23 09:46:50 +0300

    Add login screen

---
+++ src/Router.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native Router
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './screens/Login';
+const Stack = createStackNavigator();
+
+const Router = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Router;
---
+++ src/screens/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Code Talks</Text>
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fb6c04',
+    flex: 1,
+  },
+});
