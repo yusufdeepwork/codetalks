18fcf6c075e1bfab6179207eb4f0e711e7fbc420
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-07-23 11:12:21 +0300

    Style login screen for all components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Input from '../components/Input';
+import Button from '../components/Button';
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text>Code Talks</Text>
-      <Input text="e-postanızı giriniz" />
-      <Input text="şifrenizi giriniz" isSecure />
+      <View style={styles.title_container}>
+        <Text style={styles.title}>codetalks</Text>
+      </View>
+      <View style={styles.input_container}>
+        <Input text="e-postanızı giriniz" />
+        <Input text="şifrenizi giriniz" isSecure />
+        <Button title="Giriş Yap" />
+        <Button title="Kayıt Ol" light />
+      </View>
     </View>
   );
 };
@@ -18,5 +25,19 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fb6c04',
     flex: 1,
+    justifyContent: 'center',
+  },
+  title_container: {
+    flex: 4,
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+  input_container: {
+    flex: 5,
+  },
+  title: {
+    fontSize: 30,
+    color: 'white',
   },
 });
