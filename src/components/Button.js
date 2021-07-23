42ff168e2d07e07dd77ba88c6ea916b6801f69fe
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-07-23 11:11:25 +0300

    Add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+
+function Button({title, onPress, light}) {
+  return (
+    <TouchableOpacity
+      style={light ? [styles.button, styles.lightButton] : styles.button}
+      onPress={onPress}>
+      <Text style={light ? [styles.title, styles.lightTitle] : styles.title}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+export default Button;
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 10,
+    backgroundColor: '#fea040',
+    borderRadius: 8,
+  },
+  title: {
+    padding: 10,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 17,
+    color: '#ffffff',
+  },
+  lightButton: {
+    backgroundColor: '#ffffff',
+  },
+  lightTitle: {
+    color: '#fea040',
+  },
+});
